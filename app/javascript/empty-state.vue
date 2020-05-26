dc648a6c2c25c90d5a6de950b98f5aa40e599dac
LewisYoul <lewisyoul@gmail.com>
2020-05-26 09:10:36 +0100

    feature/Display empty state message when folder has no snippets

---
+++ app/javascript/empty-state.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="empty-state--wrapper">
+    <a class="empty-state--container" :href="href">
+      <img src="/icons/edit-grey.svg" width="40">
+      <p>You haven't added any snippets to this folder.</p>
+      <span>Click here to add one.</span>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    href: { type: String, required: true }
+  }
+}
+</script>
--- app/javascript/folders-show.vue
+++ app/javascript/folders-show.vue
@@ -4,12 +4,12 @@
         <back-button></back-button>
         <h2 style="margin: 2px 0px 0px 12px; display: inline-block;">{{ folder.name }}</h2>
     </div>
-    <div style="margin-top: 16px;">
-      
+    <div v-if="snippetz.length > 0" style="margin-top: 16px;">
       <snipt v-for="snippet in snippetz" @deleted="handleSnippetDeletion" :snippet="snippet" :current-folder="folder" :key="snippet.id"></snipt>
     </div>
+    <empty-state v-else :href="newSnippetUrl"></empty-state>
     <div class="folders--options-wrapper">
-      <a :href="`/snippets/new?folder_id=${folder.id}&redirect_url=/folders/${folder.id}`" class="button--cta-new">NEW SNIPPET</a>
+      <a :href="newSnippetUrl" class="button--cta-new">NEW SNIPPET</a>
     </div>
   </card>
 </template>
@@ -18,13 +18,16 @@
 import Snipt from './snipt';
 import Card from './card'
 import BackButton from './back-button'
+import EmptyState from './empty-state'
+
 import { EventBus } from './event-bus';
 
 export default {
   components: {
-    Snipt,
+    BackButton,
     Card,
-    BackButton
+    EmptyState,
+    Snipt,
   },
 
   props: {
@@ -40,6 +43,12 @@ export default {
 
   created() {
     EventBus.$on('changeSnippetFolder', this.handleSnippetFolderChange)
+  },
+
+  computed: {
+    newSnippetUrl() {
+      return `/snippets/new?folder_id=${this.folder.id}&redirect_url=/folders/${this.folder.id}`
+    }
   },
 
   methods: {
