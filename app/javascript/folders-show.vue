d15b5fd1bce675569885db6c5a4cd7dd647d09c8
LewisYoul <lewisyoul@gmail.com>
2020-08-11 08:48:52 +0100

    refactor/Use snippet preview for folder show page

--- app/javascript/folders-show.vue
+++ app/javascript/folders-show.vue
@@ -1,24 +1,27 @@
 <template>
-  <card>
-    <div style="display: flex; justify-content: space-between;">
-        <back-button></back-button>
-        <h2 style="margin: 2px 0px 0px 12px; display: inline-block;">{{ folder.name }}</h2>
+  <div class="margin-top">
+    <div style="display: flex; align-itmes: center; justify-content: space-between;">
+        <span class="inline-flex v-center">{{ folder.name }}</span>
+        <a :href="newSnippetUrl" class="button--cta-new">NEW SNIPPET</a>
     </div>
     <div v-if="snippetz.length > 0" style="margin-top: 16px;">
-      <snipt v-for="snippet in snippetz" @deleted="handleSnippetDeletion" :snippet="snippet" :current-folder="folder" :key="snippet.id"></snipt>
+      <snippet-preview
+        v-for="snippet in snippetz"
+        :key="snippet.id"
+        :snippet="snippet"
+        @deleted="handleSnippetDeletion"
+        :actionable="true"
+      />
     </div>
     <empty-state v-else :href="newSnippetUrl"></empty-state>
-    <div class="folders--options-wrapper">
-      <a :href="newSnippetUrl" class="button--cta-new">NEW SNIPPET</a>
-    </div>
-  </card>
+  </div>
 </template>
 
 <script>
-import Snipt from './snipt';
 import Card from './card'
 import BackButton from './back-button'
 import EmptyState from './empty-state'
+import SnippetPreview from './snippet-preview'
 
 import { EventBus } from './event-bus';
 
@@ -27,7 +30,7 @@ export default {
     BackButton,
     Card,
     EmptyState,
-    Snipt,
+    SnippetPreview,
   },
 
   props: {
