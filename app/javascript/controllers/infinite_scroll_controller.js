0b8a43ff158d3a485c4192c8683dadcdd9e8bb84
LewisYoul <lewisyoul@gmail.com>
2020-08-24 09:06:45 +0100

    feature/Add 80px buffer to infinite scroll trigger

--- app/javascript/controllers/infinite_scroll_controller.js
+++ app/javascript/controllers/infinite_scroll_controller.js
@@ -7,28 +7,18 @@ export default class extends Controller {
   initialize() {
     const csrfToken = document.querySelector("meta[name=csrf-token]").content
     axios.defaults.headers.common['X-CSRF-Token'] = csrfToken
+
+    this.isLoadingMore = false
   }
 
-  scroll() {
-    let nextPage = this.paginationTarget.querySelector("a[rel='next']")
-
-    if (!nextPage) { return }
-
-    let url = nextPage.href
-
-    let body = document.body
-    let html = document.documentElement
-
-    let height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight)
-
-    if (window.pageYOffset >= (height - window.innerHeight)) {
-      console.log('bottom')
-      this.loadMore(url)
-    }
+  scroll() {    
+    if (this.shouldLoadMore()) { this.loadMore() }
   }
 
-  loadMore(url) {
-    axios.get(url, { headers: { 'accept': 'application/json' } })
+  loadMore() {
+    this.isLoadingMore = true
+
+    axios.get(this.nextPageUrl, { headers: { 'accept': 'application/json' } })
       .then(res => {
         let entriesString = res.data.entries
       
@@ -38,7 +28,29 @@ export default class extends Controller {
         document.querySelectorAll('pre code').forEach((block) => {
           hljs.highlightBlock(block);
         });
+
+        this.isLoadingMore = false
       })
       .catch(console.error)
+  }
+
+  shouldLoadMore() {
+    return this.nextPageAnchor && this.hasReachedScrollTrigger() && !this.isLoadingMore
+  }
+
+  hasReachedScrollTrigger() {
+    const body = document.body
+    const html = document.documentElement
+    const height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight)
+
+    return (window.pageYOffset >= (height - window.innerHeight - 80))
+  }
+
+  get nextPageAnchor() {
+    return this.paginationTarget.querySelector("a[rel='next']")
+  }
+
+  get nextPageUrl() {
+    return this.nextPageAnchor.href
   }
 }
