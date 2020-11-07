54337ce69f0b027910472a1780c90cb9b07c9f91
LewisYoul <lewisyoul@gmail.com>
2020-11-07 12:29:40 +0000

    feature/Use marked for syntax highlighting

--- app/javascript/controllers/highlight_controller.js
+++ app/javascript/controllers/highlight_controller.js
@@ -1,16 +1,30 @@
 import { Controller } from 'stimulus';
+import marked from 'marked';
 
 export default class extends Controller {
   static targets = ["code", "languages", "highlightedBody"];
 
   connect() {
     this.addLanguagesToSelect()
+
+    marked.setOptions({
+      highlight: function(code, lang) {
+        console.log('lang', lang)
+        if (lang && lang !== '') {
+          return hljs.highlight(lang, code).value;
+        } else {
+          return hljs.highlightAuto(code).value;
+        }
+      }
+    });
+    
   }
 
   //  TODO: Pass in the selected language!!!
   highlight(event) {
     const rawCode = event.currentTarget.value
-    const highlightedCode = hljs.highlight('ruby', rawCode).value
+    console.log(marked(rawCode))
+    const highlightedCode = marked(rawCode)
 
     this.codeTarget.innerHTML = highlightedCode
     this.highlightedBodyTarget.value = highlightedCode
