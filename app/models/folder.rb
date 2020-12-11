cef8ba7295272e55915a53c35c0df54afb9eb3f5
LewisYoul <lewisyoul@gmail.com>
2020-12-11 08:48:33 +0000

    feature/Use create snippet modal from folders index page

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -6,7 +6,7 @@ class Folder < ApplicationRecord
   def popover_options_for(user)
     options = []
 
-    options << { type: :link, title: 'Add snippet', url: new_snippet_path }
+    options << { type: :modal, title: 'Add snippet', url: new_snippet_modal_snippets_path(folder_id: id) }
     options << { type: :link, title: 'Rename folder', url: edit_folder_path(self) }
 
     if snippets.empty?
