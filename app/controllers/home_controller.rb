40f547f43a9939abb73d5778bff226f4816c64bc
LewisYoul <lewisyoul@gmail.com>
2020-12-30 11:24:01 +0000

    feature/Add ability to filter connectable users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -32,23 +32,23 @@ class HomeController < ApplicationController
 
     @users = if user_signed_in?
       current_user.not_following
+      .paginate(page: params[:page] || 1, per_page: 8)
     else
       User.order(updated_at: :desc)
+      .paginate(page: params[:page] || 1, per_page: 8)
     end
 
-    # @users = current_user.not_following
+    # TODO: Extract this logic to model/service
+    @users = @users.where('name ILIKE ?', "%#{params[:search]}%") if params[:search]
 
-    # @users = @users
-    #   .order(created_at: :desc)
-    #   .paginate(page: params[:page] || 1, per_page: 15)
-
-    # respond_to do |format|
-    #   format.json do
-    #     render json: {
-    #       entries: render_to_string(partial: 'snippets/snippets', formats: [:html]),
-    #       pagination: view_context.will_paginate(@users)
-    #     }
-    #   end
-    # end
+    respond_to do |format|
+      format.html
+      format.json do
+        render json: {
+          entries: render_to_string(partial: 'users/follow_previews', formats: [:html]),
+          pagination: view_context.will_paginate(@users)
+        }
+      end
+    end
   end
 end
