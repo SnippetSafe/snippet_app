1204442d88cb779bfbc22e045ee640784b3165c9
LewisYoul <lewisyoul@gmail.com>
2021-01-09 10:59:25 +0000

    config/Sendgrid fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,17 +62,31 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "https://www.snippetsafe.com" }
-  config.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'snippetsafe.com',
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.default_url_options = { host: "https://www.snippetsafe.com" }
+  # config.smtp_settings = {
+  #   :user_name => ENV['SENDGRID_USERNAME'],
+  #   :password => ENV['SENDGRID_PASSWORD'],
+  #   :domain => 'snippetsafe.com',
+  #   :address => 'smtp.sendgrid.net',
+  #   :port => 587,
+  #   :authentication => :plain,
+  #   :enable_starttls_auto => true
+  # }
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'snippetsafe.com' }
+  config.action_mailer.perform_deliveries = true
+  ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
-    :enable_starttls_auto => true
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'heroku.com'
   }
+  
+  ActionMailer::Base.delivery_method = :smtp
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
