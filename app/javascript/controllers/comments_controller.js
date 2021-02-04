243adc28ad32f568a03729f7a6ff8ded6aa571e9
LewisYoul <lewisyoul@gmail.com>
2021-02-04 08:04:22 +0000

    feature/Prevent comment submission if empty

--- app/javascript/controllers/comments_controller.js
+++ app/javascript/controllers/comments_controller.js
@@ -1,19 +1,8 @@
 import { Controller } from 'stimulus';
-import { events } from "../mixins/events";
 
 export default class extends Controller {
   static targets = ["entries", "body"];
   
-  connect() {
-    events(this)
-  }
-
-  update(event) {
-    const element = document.getElementById(event.detail.client_id)
-
-    if (element) { element.outerHTML = event.detail.element }
-  }
-
   onCreateSuccess(event) {
     const [data, status, xhr] = event.detail;
     
