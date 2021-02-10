385c6ed5c84f0d51476d6bb66e4313315d05a73c
LewisYoul <lewisyoul@gmail.com>
2021-02-10 18:11:36 +0000

    feature/Create notification for likes

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,9 +1,19 @@
 class Like < ApplicationRecord
   belongs_to :snippet, counter_cache: true
   belongs_to :user
+  has_many :notifications, as: :notifiable, dependent: :destroy
 
   def self.toggle(user_id, snippet_id)
     like = Like.find_or_initialize_by(user_id: user_id, snippet_id: snippet_id)
-    like.persisted? ? like.destroy : like.tap(&:save!)
+
+    if like.persisted?
+      like.destroy
+    else
+      like.save!
+
+      unless user_id.to_i == like.snippet.user_id
+        like.notifications.create!(user_id: like.snippet.user_id)
+      end
+    end
   end
 end
