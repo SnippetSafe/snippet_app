43d833fadeb2e48a9a523802e0832baa02017a34
Lewis Youl <lewisyoul@lewiss-mbp.home>
2021-03-21 13:29:02 +0000

    fix: specify format for popover partial

--- app/controllers/popovers/users_controller.rb
+++ app/controllers/popovers/users_controller.rb
@@ -2,6 +2,6 @@ class Popovers::UsersController < ApplicationController
   def index
     @popover_options = current_user.popover_options
 
-    render partial: 'shared/popover', layout: false
+    render partial: 'shared/popover', layout: false, formats: [:html]
   end
 end
