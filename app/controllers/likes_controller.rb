3235f33af08f3e4a7113f968abefc30819a7d8a6
Lewis Youl <lewisyoul@lewiss-mbp.home>
2021-03-21 17:23:40 +0000

    fix: add html format to like button endpoint

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,7 @@ class LikesController < ApplicationController
   def create
     like = Like.toggle(like_params[:user_id], like_params[:snippet_id])
 
-    render partial: 'shared/like_button', locals: { snippet: Snippet.find(like_params[:snippet_id])}
+    render partial: 'shared/like_button', formats: [:html], locals: { snippet: Snippet.find(like_params[:snippet_id])}
   end
 
   private
