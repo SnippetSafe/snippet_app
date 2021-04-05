03556480b06d3e6ab3e7c93e1a2be3ab21d32a08
Lewis Youl <lewisyoul@lewiss-mbp.home>
2021-04-05 17:50:59 +0100

    fix: remove devise confirmation skip

--- app/models/user.rb
+++ app/models/user.rb
@@ -160,7 +160,6 @@ class User < ApplicationRecord
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
       user.name = unique_name(parsed_name(auth.info.nickname)) # github display name
-      user.skip_confirmation! # github already validates emails
     end
     
     if is_new_user && user.persisted?
